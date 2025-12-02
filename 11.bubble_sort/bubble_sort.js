886bb3a357806871c810425d713c9717802b7b4b
rajashree <rajashree@udmglobal.com>
2025-12-02 16:49:09 +0530

    bubble sort exercise

---
+++ 11.bubble_sort/bubble_sort.js
@@ -0,0 +1,23 @@
+
+function bubbleSort(arr) {
+ 
+  for(let i = arr.length; i > 0; i--) {
+     let swaped = false;
+    for(let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j+1]) {
+        [arr[j+1], arr[j]] = [arr[j], arr[j+1]];
+        swaps = true;
+      }
+     
+    }
+
+    if(!swaps) {
+        break;
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort([5,4,2,3,1]));
+
+// node bubble_sort.js
