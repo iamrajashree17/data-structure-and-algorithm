e52285c961add3ffe3756d2764efe6473b8eb51c
rajashree <rajashree@udmglobal.com>
2026-03-03 17:29:02 +0530

    Add solutions for Kth Smallest Element in a BST, Maximum Width of Binary Tree, and Check Completeness of a Binary Tree with example cases for each implementation.

---
+++ 32.month_three/week10/day-67/230.js
@@ -0,0 +1,33 @@
+/* 230. Kth Smallest Element in a BST
+Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+Example 1:
+Input: root = [3,1,4,null,2], k = 1
+Output: 1
+
+Example 2:
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+Output: 3
+*/
+
+function kthSmallest(root, k) {
+    let count = 0;
+    let res = 0;
+
+    function inorder(node) {
+        if(!node) return;
+
+        inorder(node.left);
+
+        count++;
+        if(count === k) {
+            res = node.val;
+            return;
+        }
+
+        inorder(node.right);
+    }
+
+    inorder(root);
+    return res;
+}
